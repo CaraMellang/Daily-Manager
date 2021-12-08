8d4a3466babd1331e3a31de92a898cb35c811b1c
Mellang <dlwpgh23@naver.com>
2021-12-08 18:14:49 +0900

    build CI

--- src/lib/HttpPath.tsx
+++ src/lib/HttpPath.tsx
@@ -1,6 +1,6 @@
 export const backPath = "https://projectdm.herokuapp.com/";
 
-// 1 is use Localhost , 0 is use BackPath
+// 1 is use Localhost , 0 is use BackPaths
 export const useLocalOn = 0;
 
 export const localPath = "http://localhost:5000";
